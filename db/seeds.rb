72e483c7a37c9d91df128e6771b48abaaa552b60
Yves Hurdequint <hurdequinty@hotmail.com>
2021-07-21 12:32:13 +0800

    seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying previous instances"
+Restaurant.destroy_all
+
+puts "Creating 5 instances of restaurant"
+
+Restaurant.new(
+  name: "Djerba la douce",
+  category: "french",
+  address: "Rue du Capitole",
+).save!
+Restaurant.new(
+  name: "Chez Fonfon",
+  category: "french",
+  address: "Port de Marseille",
+).save!
+Restaurant.new(
+  name: "La Volla",
+  category: "italian",
+  address: "Place Saint-Pierre",
+).save!
+Restaurant.new(
+  name: "Tyger Sushi",
+  category: "japanese",
+  address: "Beach Street",
+).save!
+Restaurant.new(
+  name: "Chez Momo",
+  category: "belgian",
+  address: "Rue du stade",
+).save!
