5b12f1fd13dca97665ba4144ae46609e4b6eba4a
Yves Hurdequint <hurdequinty@hotmail.com>
2021-07-21 12:03:38 +0800

    model created, migrated, and validation passed rake

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true
+
+  validates :category, inclusion: CATEGORIES
+end
